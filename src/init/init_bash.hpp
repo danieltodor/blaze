05f33c922a8e440c6e62b703d4124caadad268c0
Daniel Todor <user@example.com>
2022-12-22 20:16:24 +0100

    only header file

--- src/init/init_bash.hpp
+++ src/init/init_bash.hpp
@@ -1,6 +1,35 @@
 #ifndef BLAZE_INIT_INIT_BASH_HPP_INCLUDED
 #define BLAZE_INIT_INIT_BASH_HPP_INCLUDED
 
-void init_bash();
+#include <iostream>
+
+void init_bash()
+{
+    std::cout << R"d(
+save_start_time() {
+    date +%s.%N > "/dev/shm/${USER}.bashtime.${SESSION}"
+}
+
+get_start_time() {
+    echo "$(cat /dev/shm/${USER}.bashtime.${SESSION})"
+}
+
+get_finish_time() {
+    echo "$(date +%s.%N)"
+}
+
+run_on_exit() {
+    rm /dev/shm/${USER}.bashtime.${SESSION}
+}
+trap run_on_exit EXIT
+
+SESSION=$BASHPID
+save_start_time
+
+PROMPT_COMMAND="echo"
+PS0='$(save_start_time)'
+PS1='$(blaze bash $(get_start_time) $(get_finish_time))'
+    )d";
+}
 
 #endif
