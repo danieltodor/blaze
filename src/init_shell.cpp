74303e284d941b1551279fd68304f5f5cf69c699
Daniel Todor <user@example.com>
2022-12-23 15:23:46 +0100

    hashmap refactored

--- src/init_shell.cpp
+++ src/init_shell.cpp
@@ -1,11 +1,10 @@
-#include <unordered_map>
-
 #include "init_shell.hpp"
 #include "init/init_bash.hpp"
 
 void init_shell(std::string shell)
 {
-    std::unordered_map<std::string, void (*)()> shells;
-    shells["bash"] = &init_bash;
-    shells[shell]();
+    if (shell == "bash")
+    {
+        init_bash();
+    }
 }
