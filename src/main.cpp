4be334d16d0624f5f6db1e260f5919c8d84994a3
Daniel Todor <user@example.com>
2022-12-23 15:40:51 +0100

    return if no arguments passed

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,10 @@
 
 int main(int argc, char *argv[])
 {
+    if (argc == 1)
+    {
+        return 0;
+    }
     std::unordered_map<int, std::string *> args;
     std::string first, second, third;
     args[1] = &first;
