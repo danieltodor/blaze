24f68e08c1b5ae60dbe6da7fc22be023a1499d87
Daniel Todor <user@example.com>
2023-02-16 18:54:25 +0100

    use only one hashmap for colors

--- src/color.cpp
+++ src/color.cpp
@@ -6,8 +6,7 @@ const std::string csi = "\001\033["; // Control Sequence Introducer
 const std::string rgb_foreground_prefix = "38;2;";
 const std::string rgb_background_prefix = "48;2;";
 
-typedef std::unordered_map<std::string, std::string> stringmap;
-const stringmap foreground_color_map = {
+const std::unordered_map<std::string, std::string> foreground_color_map = {
     {"black", std::to_string(BLACK)},
     {"red", std::to_string(RED)},
     {"green", std::to_string(GREEN)},
@@ -16,16 +15,6 @@ const stringmap foreground_color_map = {
     {"magenta", std::to_string(MAGENTA)},
     {"cyan", std::to_string(CYAN)},
     {"white", std::to_string(WHITE)}
-};
-const stringmap background_color_map = {
-    {"black", std::to_string(BLACK + 10)},
-    {"red", std::to_string(RED + 10)},
-    {"green", std::to_string(GREEN + 10)},
-    {"yellow", std::to_string(YELLOW + 10)},
-    {"blue", std::to_string(BLUE + 10)},
-    {"magenta", std::to_string(MAGENTA + 10)},
-    {"cyan", std::to_string(CYAN + 10)},
-    {"white", std::to_string(WHITE + 10)}
 };
 
 std::string graphics(const std::string &code)
@@ -36,18 +25,20 @@ std::string graphics(const std::string &code)
     return result;
 }
 
-std::string to_color_code(const std::string &color, const stringmap &color_map, const std::string &rgb_prefix)
+std::string to_color_code(const std::string &color, const int offset = 0)
 {
     if (color.empty())
     {
         return "";
     }
-    else if (color_map.find(color) != color_map.end())
+    else if (foreground_color_map.find(color) != foreground_color_map.end())
     {
-        return graphics(color_map.at(color));
+        const int color_code = std::stoi(foreground_color_map.at(color));
+        return graphics(std::to_string(color_code + offset));
     }
     else
     {
+        const std::string rgb_prefix = offset == 0 ? rgb_foreground_prefix : rgb_background_prefix;
         return graphics(rgb_prefix + color);
     }
 }
@@ -59,12 +50,12 @@ std::string set_text_mode(const int code)
 
 std::string set_foreground(const std::string &color)
 {
-    return to_color_code(color, foreground_color_map, rgb_foreground_prefix);
+    return to_color_code(color);
 }
 
 std::string set_background(const std::string &color)
 {
-    return to_color_code(color, background_color_map, rgb_background_prefix);
+    return to_color_code(color, BACKGROUND_COLOR_OFFSET);
 }
 
 std::string reset_all()
