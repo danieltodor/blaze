c94a4732c899dbf6e7222845ba909485500b68d4
Daniel Todor <user@example.com>
2023-03-18 17:07:52 +0100

    using local vars in function

--- src/init/init.bash
+++ src/init/init.bash
@@ -1,15 +1,16 @@
 # Get the RGB color of the terminal emulator. Works only in xterm compatible emulators.
 blaze_get_current_background() {
     exec < /dev/tty
-    oldstty=$(stty -g)
+    local oldstty=$(stty -g)
     stty raw -echo min 0
-    col=11 # background
-    echo -en "\033]${col};?\033\\" > /dev/tty
+    local ps=11 # background
+    echo -en "\033]${ps};?\033\\" > /dev/tty
+    local hex_rgb=""
     IFS=';' read -r -d '\' hex_rgb
     stty $oldstty
     hex_rgb=$(echo $hex_rgb | sed 's/^.*\;//;s/[^rgb:0-9a-f/]//g')
     hex_rgb=$(echo $hex_rgb | sed 's/[rgb:|/]//g')
-    bg_rgb=
+    local bg_rgb=""
     bg_rgb+=$((16#${hex_rgb:0:4} >> 8))";"
     bg_rgb+=$((16#${hex_rgb:4:4} >> 8))";"
     bg_rgb+=$((16#${hex_rgb:8:4} >> 8))
