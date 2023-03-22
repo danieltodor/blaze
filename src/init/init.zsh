449f8ae38e27dceead451ad025ec3b2eed2c23e3
Daniel Todor <user@example.com>
2023-03-22 20:03:06 +0100

    zsh init simplified

--- src/init/init.zsh
+++ src/init/init.zsh
@@ -22,45 +22,27 @@ blaze_get_current_time() {
     echo "$(date +%s.%N)"
 }
 
-blaze_save_start_time() {
-    echo $(blaze_get_current_time) > $blaze_time_file
+blaze_preexec() {
+    blaze_start_time=$(blaze_get_current_time)
 }
 
-blaze_get_start_time() {
-    echo "$(cat $blaze_time_file)"
+blaze_precmd() {
+    blaze_exit_status=$?
+    blaze_finish_time=$(blaze_get_current_time)
 }
-
-blaze_save_exit_status() {
-    echo $? > $blaze_exit_status_file
-}
-
-blaze_get_exit_status() {
-    echo "$(cat $blaze_exit_status_file)"
-}
-
-# TODO: Create a similar trap for zsh
-# TRAPEXIT() {
-#     rm $blaze_time_file
-#     rm $blaze_exit_status_file
-#     return $(( 128 + $1 ))
-# }
-
-blaze_session="${USER}_${PPID}"
-
-if [[ -d "/dev/shm" ]]; then
-    blaze_file_prefix="/dev/shm/$blaze_session"
-else
-    blaze_file_prefix="/tmp/$blaze_session"
-fi
 
 blaze_default_background=$(blaze_get_current_background)
-blaze_time_file="${blaze_file_prefix}_time"
-blaze_exit_status_file="${blaze_file_prefix}_exit_status"
+blaze_start_time=""
+blaze_finish_time=""
+blaze_exit_status=""
+
+blaze_preexec
+blaze_precmd
 
 autoload -Uz add-zsh-hook
-add-zsh-hook preexec blaze_save_start_time
-add-zsh-hook precmd blaze_save_exit_status
+add-zsh-hook preexec blaze_preexec
+add-zsh-hook precmd blaze_precmd
 
 setopt prompt_subst # Enable prompt substitution
 
-PROMPT='$(blaze zsh -s $(blaze_get_start_time) -f $(blaze_get_current_time) -e $(blaze_get_exit_status) -b $blaze_default_background)'
+PROMPT='$(blaze zsh -s $blaze_start_time -f $blaze_finish_time -e $blaze_exit_status -b $blaze_default_background)'
