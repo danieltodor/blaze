abef4133b5dd6c28882cd02465d06c43eb2213b4
Daniel Todor <user@example.com>
2024-07-28 20:20:32 +0200

    keep original prompt variables

--- src/init/init.bash
+++ src/init/init.bash
@@ -24,7 +24,7 @@ _blaze_run_on_exit() {
 }
 trap _blaze_run_on_exit EXIT
 
-_blaze_session="${USER}_${BASHPID}"
+_blaze_session="_blaze_${USER}_${BASHPID}"
 
 if [[ -d "/dev/shm" ]]; then
     _blaze_file_prefix="/dev/shm/$_blaze_session"
@@ -40,7 +40,7 @@ _blaze_save_start_time
 _blaze_save_exit_status
 
 # Bash doesn`t have preexec/precmd hooks, but the behavior of these prompt variables are close enough
-PS0='$(_blaze_save_start_time)' # Is expanded after a command is read and before the command is executed
-PROMPT_COMMAND='$(_blaze_save_exit_status)' # Executed before the printing of each primary prompt
+PS0=$PS0'$(_blaze_save_start_time)' # Is expanded after a command is read and before the command is executed
+PROMPT_COMMAND=$PROMPT_COMMAND'$(_blaze_save_exit_status)' # Executed before the printing of each primary prompt
 
 PS1='$(blaze bash --prompt -s $(_blaze_get_start_time) -f $(_blaze_get_current_time) -e $(_blaze_get_exit_status) -b $_blaze_default_background)'
