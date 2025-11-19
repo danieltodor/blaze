214e4b8a67a5bcaecd5bf1992a29a4dbf4f869f9
Daniel Todor <user@example.com>
2025-11-19 20:27:29 +0100

    separate pool implementation

---
+++ src/pool.cpp
@@ -0,0 +1,18 @@
+#include "pool.hpp"
+
+#define THREAD_LIMIT 4U
+
+BS::light_thread_pool thread_pool(std::min(std::thread::hardware_concurrency(), THREAD_LIMIT));
+
+// ----------------------------------- TESTS -----------------------------------
+#ifdef TEST
+#include "test.hpp"
+
+TEST_CASE("thread count")
+{
+    const std::size_t thread_count = thread_pool.get_thread_count();
+    CHECK(thread_count > 0);
+    CHECK(thread_count <= THREAD_LIMIT);
+}
+
+#endif
--- src/pool.hpp
+++ src/pool.hpp
@@ -2,19 +2,4 @@
 
 #include "thread-pool/BS_thread_pool.hpp"
 
-#define THREAD_LIMIT 4U
-
-inline BS::thread_pool thread_pool(std::min(std::thread::hardware_concurrency(), THREAD_LIMIT));
-
-// ----------------------------------- TESTS -----------------------------------
-#ifdef TEST
-#include "test.hpp"
-
-TEST_CASE("thread count")
-{
-    const std::size_t thread_count = thread_pool.get_thread_count();
-    CHECK(thread_count > 0);
-    CHECK(thread_count <= THREAD_LIMIT);
-}
-
-#endif
+extern BS::light_thread_pool thread_pool;
